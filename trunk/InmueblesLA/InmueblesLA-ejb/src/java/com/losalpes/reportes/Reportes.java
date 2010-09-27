bb9068cd43bcb8d9efa5248e88b29a1519d75300
ans.gomez@gmail.com <ans.gomez@gmail.com@00bf1dc0-23b0-af8e-5d4f-5a83dd947d13>
2010-09-27 18:00:18 +0000

    actualizacion de javadoc

--- trunk/InmueblesLA/InmueblesLA-ejb/src/java/com/losalpes/reportes/Reportes.java
+++ trunk/InmueblesLA/InmueblesLA-ejb/src/java/com/losalpes/reportes/Reportes.java
@@ -10,7 +10,8 @@ import java.util.ArrayList;
 import javax.ejb.Remote;
 
 /**
- *
+ * Esta es la interfaz del bean que genera reportes. por el momento
+ * solo hay reportes de inmuebles
  * @author andres
  */
 @Remote
