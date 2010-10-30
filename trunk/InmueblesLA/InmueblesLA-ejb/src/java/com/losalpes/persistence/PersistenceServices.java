cfca53edc2890ef3904e2a5348e2f4dddb5aba44
juana.valenzuela45 <juana.valenzuela45@00bf1dc0-23b0-af8e-5d4f-5a83dd947d13>
2010-10-30 16:04:22 +0000

    Persistencia

---
+++ trunk/InmueblesLA/InmueblesLA-ejb/src/java/com/losalpes/persistence/PersistenceServices.java
@@ -0,0 +1,65 @@
+/**
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * $Id$ PersistenceServices.java
+ * Universidad de los Andes (Bogotá - Colombia)
+ * Departamento de Ingeniería de Sistemas y Computación
+ * Licenciado bajo el esquema Academic Free License version 2.1
+ *
+ * Ejercicio: Muebles los Alpes
+ * Autor: Camilo Alvarez
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+package com.losalpes.persistence;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * Implementación de los servicios de persistencia que se le prestan al sistema.
+ * @author Camilo Alvarez
+ */
+@Stateless
+public class PersistenceServices implements IPersistenceServices {
+
+    // -----------------------------------------------
+    // Atributos
+    // -----------------------------------------------
+
+    /**
+     * Instancia del entity manager provista por el contenedor.
+     */
+    @PersistenceContext
+    private EntityManager em;
+
+    // -----------------------------------------------
+    // Constructor
+    // -----------------------------------------------
+
+    /**
+     * Constructor de la clase.
+     */
+    public PersistenceServices() {
+    }
+
+    public void create(Object obj) {
+        em.persist(obj);
+    }
+
+    public void update(Object obj) {
+        em.merge(obj);
+    }
+
+    public void delete(Object obj) {
+        em.remove(em.merge(obj));
+    }
+
+    public List findAll(Class c) {
+        return em.createQuery("select O from " + c.getSimpleName() + " as O").getResultList();
+    }
+
+    public Object findById(Class c, Object id) {
+        return em.find(c, id);
+    }
+}
