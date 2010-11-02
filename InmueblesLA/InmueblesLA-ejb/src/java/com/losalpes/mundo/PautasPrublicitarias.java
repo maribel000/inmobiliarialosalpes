aaa2e4ad29b6c718da07bd9ef58b8a8b803448d3
danielfelipepalaciosgodoy <danielfelipepalaciosgodoy@00bf1dc0-23b0-af8e-5d4f-5a83dd947d13>
2010-11-02 23:06:10 +0000

    Persistence

---
+++ InmueblesLA/InmueblesLA-ejb/src/java/com/losalpes/mundo/PautasPrublicitarias.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.losalpes.mundo;
+
+/**
+ *
+ * @author df.palacios30
+ */
+public class PautasPrublicitarias {
+
+    private String nombreEmpresa;
+
+    private String descripcion;
+
+    private int telefono;
+
+    private String email;
+
+    public PautasPrublicitarias()
+    {
+        
+    }
+
+
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+}
