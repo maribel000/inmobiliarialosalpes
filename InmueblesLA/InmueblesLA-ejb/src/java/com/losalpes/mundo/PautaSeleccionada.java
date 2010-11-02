ceb26abcf017013e64c40f3e0320f1bf4b8bd342
danielfelipepalaciosgodoy <danielfelipepalaciosgodoy@00bf1dc0-23b0-af8e-5d4f-5a83dd947d13>
2010-11-02 23:56:13 +0000

    Persistence Inmueble

---
+++ InmueblesLA/InmueblesLA-ejb/src/java/com/losalpes/mundo/PautaSeleccionada.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.losalpes.mundo;
+
+/**
+ *
+ * @author df.palacios30
+ */
+public class PautaSeleccionada {
+
+    private int idPautaSeleccionada;
+
+    private int idpautacatalogo;
+
+    private int idpautalogin;
+
+
+
+    public int getIdPautaSeleccionada() {
+        return idPautaSeleccionada;
+    }
+
+    public void setIdPautaSeleccionada(int idPautaSeleccionada) {
+        this.idPautaSeleccionada = idPautaSeleccionada;
+    }
+
+    public int getIdpautacatalogo() {
+        return idpautacatalogo;
+    }
+
+    public void setIdpautacatalogo(int idpautacatalogo) {
+        this.idpautacatalogo = idpautacatalogo;
+    }
+
+    public int getIdpautalogin() {
+        return idpautalogin;
+    }
+
+    public void setIdpautalogin(int idpautalogin) {
+        this.idpautalogin = idpautalogin;
+    }
+
+}
