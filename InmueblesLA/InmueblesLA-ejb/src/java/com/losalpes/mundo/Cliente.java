ecd48bfd7fe21d38b2830d4890c28febd86ddaf1
danielfelipepalaciosgodoy <danielfelipepalaciosgodoy@00bf1dc0-23b0-af8e-5d4f-5a83dd947d13>
2010-11-05 12:20:10 +0000

    usuarios

--- InmueblesLA/InmueblesLA-ejb/src/java/com/losalpes/mundo/Cliente.java
+++ InmueblesLA/InmueblesLA-ejb/src/java/com/losalpes/mundo/Cliente.java
@@ -34,46 +34,12 @@ public class Cliente implements ICliente
     /**
      * Contiene el documento de identificacion del cliente
      */
-    private String documentoIdentificacion;
-
-    /**
-     * Contiene el tipo de documento del cliente
-     */
-    private String tipoDocumento;
-
-    /**
-     * Contiene el telefono de residencia del cliente
-     */
-    private String telefonoResidencia;
-
-    /**
-     * Contiene el telefono celular del cliente
-     */
-    private String telefonoCelular;
-
-    /**
-     * Contiene la ciudad de residencia del cliente
-     */
-    private String ciudadResidencia;
-
-    /**
-     * Contiene la direccion del cliente
-     */
-    private String direccion;
-
-    /**
-     * Contiene el pais del cliente
-     */
-    private String pais;
-    /**
-     * Contiene la profesion del cliente
-     */
-    private String profesion;
+    private String cc;
 
     /**
      * Contiene el correo electronico del cliente
      */
-    private String correoElectronico;
+    private String email;
 
     private ArrayList<IInmueble> inmueblesComprado;
 
@@ -151,30 +117,12 @@ public class Cliente implements ICliente
     }
 
     /**
-     * modifica el ciudadResidencia
-     * @return ciudadResidencia
-     */
-    public String getCiudadResidencia()
-    {
-        return ciudadResidencia;
-    }
-
-    /**
-     * modifica el ciudadResidencia actual
-     * @param ciudadResidencia
-     */
-    public void setCiudadResidencia(String ciudadResidencia)
-    {
-        this.ciudadResidencia = ciudadResidencia;
-    }
-
-    /**
      * modifica el correoElectronico
      * @return correoElectronico
      */
     public String getCorreoElectronico()
     {
-        return correoElectronico;
+        return email;
     }
 
     /**
@@ -183,25 +131,7 @@ public class Cliente implements ICliente
      */
     public void setCorreoElectronico(String correoElectronico)
     {
-        this.correoElectronico = correoElectronico;
-    }
-
-    /**
-     * modifica el direccion
-     * @return direccion
-     */
-    public String getDireccion()
-    {
-        return direccion;
-    }
-
-    /**
-     * modifica el direccion actual
-     * @param direccion
-     */
-    public void setDireccion(String direccion)
-    {
-        this.direccion = direccion;
+        this.email = correoElectronico;
     }
 
     /**
@@ -210,7 +140,7 @@ public class Cliente implements ICliente
      */
     public String getDocumentoIdentificacion()
     {
-        return documentoIdentificacion;
+        return cc;
     }
 
     /**
@@ -219,7 +149,7 @@ public class Cliente implements ICliente
      */
     public void setDocumentoIdentificacion(String documentoIdentificacion)
     {
-        this.documentoIdentificacion = documentoIdentificacion;
+        this.cc = documentoIdentificacion;
     }
 
     /**
@@ -238,96 +168,6 @@ public class Cliente implements ICliente
     public void setNombre(String nombre)
     {
         this.nombre = nombre;
-    }
-
-    /**
-     * modifica el pais
-     * @return pais
-     */
-    public String getPais()
-    {
-        return pais;
-    }
-
-    /**
-     * modifica el pais actual
-     * @param pais
-     */
-    public void setPais(String pais)
-    {
-        this.pais = pais;
-    }
-
-    /**
-     * modifica el profesion
-     * @return profesion
-     */
-    public String getProfesion()
-    {
-        return profesion;
-    }
-
-    /**
-     * modifica el profesion actual
-     * @param profesion
-     */
-    public void setProfesion(String profesion)
-    {
-        this.profesion = profesion;
-    }
-
-    /**
-     * modifica el telefonoCelular
-     * @return telefonoCelular
-     */
-    public String getTelefonoCelular()
-    {
-        return telefonoCelular;
-    }
-
-    /**
-     * modifica el telefonoCelular actual
-     * @param telefonoCelular
-     */
-    public void setTelefonoCelular(String telefonoCelular)
-    {
-        this.telefonoCelular = telefonoCelular;
-    }
-
-    /**
-     * modifica el telefonoResidencia
-     * @return telefonoResidencia
-     */
-    public String getTelefonoResidencia()
-    {
-        return telefonoResidencia;
-    }
-
-    /**
-     * modifica el telefonoResidencia actual
-     * @param telefonoResidencia
-     */
-    public void setTelefonoResidencia(String telefonoResidencia)
-    {
-        this.telefonoResidencia = telefonoResidencia;
-    }
-
-    /**
-     * modifica el tipoDocumento
-     * @return tipoDocumento
-     */
-    public String getTipoDocumento()
-    {
-        return tipoDocumento;
-    }
-
-    /**
-     * modifica el tipoDocumento actual
-     * @param tipoDocumento
-     */
-    public void setTipoDocumento(String tipoDocumento)
-    {
-        this.tipoDocumento = tipoDocumento;
     }
 
     public String getLogIn() {
