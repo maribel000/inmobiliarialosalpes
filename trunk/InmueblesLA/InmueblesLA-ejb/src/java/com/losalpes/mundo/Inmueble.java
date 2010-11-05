8d8a036bfd2e4c3a7213f74dc6576dda7377de60
danielfelipepalaciosgodoy <danielfelipepalaciosgodoy@00bf1dc0-23b0-af8e-5d4f-5a83dd947d13>
2010-11-05 12:41:25 +0000

    Inmuebles

--- trunk/InmueblesLA/InmueblesLA-ejb/src/java/com/losalpes/mundo/Inmueble.java
+++ trunk/InmueblesLA/InmueblesLA-ejb/src/java/com/losalpes/mundo/Inmueble.java
@@ -28,65 +28,40 @@ public class Inmueble implements IInmueble
      * Contiene la descripcion del inmueble
      */
     private String descripcion;
-    
-    /**
-     * Contiene si inmueble es interior o exterior
-     * interior = true
-     * exterior = false
-     */
-    private boolean interiorExterior;
-    
-    /**
-     * Contiene el material del inmueble
-     */
-    private String material;
-    
-    /**
-     * Contiene el alto del inmueble
-     */
-    private int alto;
-    
-    /**
-     * Contiene el ancho del inmueble
-     */
-    private int ancho;
-    
-    /**
-     * Contiene la profundidad del inmueble
-     */
-    private int profundidad;
-    
-    /**
-     * Contiene el color del inmueble
-     */
-    private String color;
-    
-    /**
-     * Contiene el peso del inmueble
-     */
-    private int peso;
-    
-    /**
-     * Contiene la foto del inmueble
-     */
-    private String foto;
 
     /**
      * tipo del inmueble
      */
     private String tipo;
 
+    /**
+     * logIn vendedor
+     */
+    private String logInVendedor;
+
+    /**
+     * losIn comprador
+     */
+    private String comprador;
+
     // ---------------------------------------
     // Constructor
     // ---------------------------------------
     /**
-     * Constructor de la clase
+     * Constructor de la clase vacio
      */
     public Inmueble()
     {
         
     }
 
+    /**
+     * con parametros por defecto
+     * @param ref
+     * @param nom
+     * @param desc
+     * @param ntipo
+     */
     public Inmueble(int ref, String nom, String desc, String ntipo )
     {
         idInmueble = ref;
@@ -102,60 +77,6 @@ public class Inmueble implements IInmueble
     // ---------------------------------------
     // Metodos retorno
     // ---------------------------------------
-
-    /**
-     * retorna el alto del inmueble
-     * @return alto
-     */
-    public int getAlto()
-    {
-        return alto;
-    }
-
-    /**
-     * modifica el alto del inmueble
-     * @param alto
-     */
-    public void setAlto(int alto)
-    {
-        this.alto = alto;
-    }
-
-    /**
-     * retorna el ancho del inmueble
-     * @return ancho
-     */
-    public int getAncho()
-    {
-        return ancho;
-    }
-
-    /**
-     * modifica el ancho del inmueble
-     * @param ancho
-     */
-    public void setAncho(int ancho)
-    {
-        this.ancho = ancho;
-    }
-
-    /**
-     * retorna el color del inmueble
-     * @return color
-     */
-    public String getColor()
-    {
-        return color;
-    }
-
-    /**
-     * modifica el color del inmueble
-     * @param color
-     */
-    public void setColor(String color)
-    {
-        this.color = color;
-    }
 
     /**
      * retorna la descripcion del inmueble
@@ -176,60 +97,6 @@ public class Inmueble implements IInmueble
     }
 
     /**
-     * retorna la foto del inmueble
-     * @return foto
-     */
-    public String getFoto()
-    {
-        return foto;
-    }
-
-    /**
-     * modifica la foto del inmueble
-     * @param foto
-     */
-    public void setFoto(String foto)
-    {
-        this.foto = foto;
-    }
-
-    /**
-     * retorna el interiorExterior del inmueble
-     * @return interiorExterior
-     */
-    public boolean isInteriorExterior()
-    {
-        return interiorExterior;
-    }
-
-    /**
-     * modifica el interiorExterior del inmueble
-     * @param interiorExterior
-     */
-    public void setInteriorExterior(boolean interiorExterior)
-    {
-        this.interiorExterior = interiorExterior;
-    }
-
-    /**
-     * retorna el material del inmueble
-     * @return material
-     */
-    public String getMaterial()
-    {
-        return material;
-    }
-
-    /**
-     * modifica el material del inmueble
-     * @param material
-     */
-    public void setMaterial(String material)
-    {
-        this.material = material;
-    }
-
-    /**
      * retorna el nombre del inmueble
      * @return nombre
      */
@@ -247,41 +114,7 @@ public class Inmueble implements IInmueble
         this.nombre = nombre;
     }
 
-    /**
-     * retorna el peso del inmueble
-     * @return peso
-     */
-    public int getPeso()
-    {
-        return peso;
-    }
-
-    /**
-     * modifica el peso del inmueble
-     * @param peso
-     */
-    public void setPeso(int peso)
-    {
-        this.peso = peso;
-    }
-
-    /**
-     * retorna la profundidad del inmueble
-     * @return profundidad
-     */
-    public int getProfundidad()
-    {
-        return profundidad;
-    }
-
-    /**
-     * modifica la profundidad del inmueble
-     * @param profundidad
-     */
-    public void setProfundidad(int profundidad)
-    {
-        this.profundidad = profundidad;
-    }
+ 
 
     /**
      * retorna la idInmueble del inmueble
@@ -301,7 +134,51 @@ public class Inmueble implements IInmueble
         this.idInmueble = idInmueble;
     }
 
-    public String getTipoInmueble() {
+    /**
+     * retorna id comprador
+     * @return
+     */
+    public String getComprador() {
+        return comprador;
+    }
+
+    /**
+     * cambia id comprador
+     * @param comprador
+     */
+    public void setComprador(String comprador) {
+        this.comprador = comprador;
+    }
+
+    /**
+     * retorna login vendedor
+     * @return
+     */
+    public String getLogInVendedor() {
+        return logInVendedor;
+    }
+
+    /**
+     * modifica id vendedor
+     * @param logInVendedor
+     */
+    public void setLogInVendedor(String logInVendedor) {
+        this.logInVendedor = logInVendedor;
+    }
+
+    /**
+     * obtiene tipo inmueble
+     * @return
+     */
+    public String getTipo() {
         return tipo;
+    }
+
+    /**
+     * retorna tipo inmueble
+     * @param tipo
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
     }
 }
