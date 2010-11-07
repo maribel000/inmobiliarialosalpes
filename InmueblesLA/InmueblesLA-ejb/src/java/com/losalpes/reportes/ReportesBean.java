b129abe5000b5fb5fc179cdb916bd98c9c9df445
danielfelipepalaciosgodoy <danielfelipepalaciosgodoy@00bf1dc0-23b0-af8e-5d4f-5a83dd947d13>
2010-11-07 15:13:34 +0000

    funcionaaa la BD corre!!!

---
+++ InmueblesLA/InmueblesLA-ejb/src/java/com/losalpes/reportes/ReportesBean.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.losalpes.reportes;
+
+import com.losalpes.entities.Inmueble;
+import java.util.ArrayList;
+import javax.ejb.Stateless;
+
+/**
+ *  Esta bean es aquel que genera los reportes, por medio de un cache
+ * @author andres
+ */
+@Stateless
+public class ReportesBean implements Reportes {
+    
+    /**
+     * Class Methods
+     */
+
+    /**
+     * Este metodo consulta el cache para devolver el reporte pedido
+     * @param id - identificador del reporte
+     * @return codigo HTML del reporte
+     */
+    public Inmueble generarReporteInmueble(String id) {
+
+        ReporteInmueble rep = CacheReporteInmueble.getReporte(id);
+
+        return rep.getInmueble();
+    }
+
+    public ArrayList generarReporteParametro(String parametro){
+        ArrayList inmuebles = new ArrayList();
+        return inmuebles;
+    }
+}
