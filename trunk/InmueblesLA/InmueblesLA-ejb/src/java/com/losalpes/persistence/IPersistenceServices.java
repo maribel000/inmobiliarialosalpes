e96dcbf75dd0b9ef47e0d654f0a90e1a516c8e32
danielfelipepalaciosgodoy <danielfelipepalaciosgodoy@00bf1dc0-23b0-af8e-5d4f-5a83dd947d13>
2010-11-08 00:08:50 +0000

    :D

--- trunk/InmueblesLA/InmueblesLA-ejb/src/java/com/losalpes/persistence/IPersistenceServices.java
+++ trunk/InmueblesLA/InmueblesLA-ejb/src/java/com/losalpes/persistence/IPersistenceServices.java
@@ -5,6 +5,10 @@
 
 package com.losalpes.persistence;
 
+import com.losalpes.entities.Inmueble;
+import com.losalpes.entities.Transacciones;
+import com.losalpes.entities.Usuario;
+import java.util.List;
 import javax.ejb.Remote;
 
 /**
@@ -14,30 +18,27 @@ import javax.ejb.Remote;
 @Remote
 public interface IPersistenceServices {
 
+    // -----------------------------------------------
+    // Métodos
+    // -----------------------------------------------
     /**
      * Permite crear un objeto dentro de la persistencia del sistema.
      * @param obj. Objeto que representa la instancia de la entidad que se quiere crear.
      */
-    public void create(java.lang.Object obj)throws Exception;
-
-    /**
-     * Permite modificar un objeto dentro de la persistencia del sistema.
-     * @param obj. Objeto que representa la instancia de la entidad que se quiere modificar.
-     */
-    public void update(java.lang.Object obj);
+    public void create(Object obj)throws Exception;
 
     /**
      * Permite borrar un objeto dentro de la persistencia del sistema.
      * @param obj. Objeto que representa la instancia de la entidad que se quiere borrar.
      */
-    public void delete(java.lang.Object obj)throws Exception;
+    public void delete(Object obj)throws Exception;
 
     /**
      * Retorna la lista de todos los elementos de una clase dada que se encuentran en el sistema.
      * @param c. Clase cuyos objetos quieren encontrarse en el sistema.
      * @return list. Listado de todos los objetos de una clase dada que se encuentran en el sistema.
      */
-    public java.util.List findAll(java.lang.Class c);
+    public List findAll(Class c);
 
     /**
      * Retorna la instancia de una entidad dado un identificador y la clase de la entidadi.
@@ -45,6 +46,64 @@ public interface IPersistenceServices {
      * @param id. Identificador unico del objeto.
      * @return obj. Resultado de la consulta.
      */
-    public java.lang.Object findById(java.lang.Class c, java.lang.Object id);
+    public Object findById(Class c, Object id);
 
+    public void update(Object obj);
+
+    // --------------------------
+    // Persistencia BD
+    // --------------------------
+
+    /**
+     * Guarda un nuevo ICliente en la BD
+     * @param cliente
+     */
+    public void persistirNuevoCliente(Usuario c);
+
+    /**
+     * Guarda un nuevo IInmueble en la BD
+     * @param nuevo
+     */
+    public void persistirNuevoInmueble( Inmueble nuevo );
+
+    /**
+     * persiste una nueva transaccion
+     * @param nueva
+     */
+    public void persistirNuevaTransaccion(Transacciones nueva);
+    /**
+     * busca los inmuebles que tengan por id el del cliente c
+     * @param c
+     */
+    public List consultarInmueblesAsociadosClienteVendedor ( Usuario c );
+    /**
+     * busca los inmuebles que tengan por id el del cliente c
+     * @param c
+     */
+    public List consultarInmueblesAsociadosClienteComprador ( Usuario c );
+    /**
+     * retorna todos los clientes
+     * @param idCliente
+     * @return
+     */
+    public List darCliente ( int idCliente );
+    /**
+     * Retorna los inmuebles dado un tipo
+     */
+    public List<Inmueble> darInmueblesPorTipo( int tipo );
+    /**
+     * registra un inmueble a un comprador
+     */
+    public void registrarInmuebleComprador(int idcomprador, int idinmueble);
+    public void createQuery7(String string, Usuario i);
+    public void createQuery6(String string, Inmueble i);
+    public void createQuery5(String string, Transacciones t);
+    public List<Inmueble> createQuery4 (String s, int idvendedor);
+    public List<Inmueble> createQuery3 (String s, int idcomprador);
+    public List<Usuario> createQuery2(String query, int idCliente);
+    public List<Inmueble> createQuery1(String string, int tipo);
+    public void createQuery0(String string, int idcomprador, int idinmueble);
+    public List<Inmueble> createQuery(String query, int idinmueble);
+
+    public void llenardatos(int a);
 }
