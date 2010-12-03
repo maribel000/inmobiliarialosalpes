fe8d45c2b0a769903969e6e5815f7684b605721e
ans.gomez <ans.gomez@00bf1dc0-23b0-af8e-5d4f-5a83dd947d13>
2010-12-03 15:33:25 +0000

    Webservice actualizado al nuevo bean

--- trunk/InmueblesLA/InmueblesLA-ejb/src/java/com/losalpes/webservices/CrearInmueble.java
+++ trunk/InmueblesLA/InmueblesLA-ejb/src/java/com/losalpes/webservices/CrearInmueble.java
@@ -28,32 +28,29 @@ public class CrearInmueble {
     private IPersistenceServices ejbRef;
     // Add business logic below. (Right-click in editor and choose
     // "Web Service > Add Operation"
-    
+
     @WebMethod(operationName = "crearInmueble")
     public boolean crearInmueble(@WebParam(name = "descripcion")
-    String descripcion, @WebParam(name = "idInmueble")
-    int idInmueble, @WebParam(name = "iddueno")
+    String descripcion, @WebParam(name = "iddueno")
     int iddueno, @WebParam(name = "idvendedor")
     int idvendedor, @WebParam(name = "nombre")
     String nombre, @WebParam(name = "tipo")
-    String tipo) {
-        return ejbRef.crearInmueble(descripcion, idInmueble, iddueno, idvendedor, nombre, tipo);
+    int tipo) {
+        return ejbRef.crearInmueble(descripcion, iddueno, idvendedor, nombre, tipo);
     }
 
     @WebMethod(operationName = "crearUsuario")
-    public boolean crearUsuario(@WebParam(name = "idUsuario")
-    int idUsuario, @WebParam(name = "apellido")
+    public boolean crearUsuario(@WebParam(name = "apellido")
     String apellido, @WebParam(name = "cc")
     String cc, @WebParam(name = "correoElectronico")
     String correoElectronico, @WebParam(name = "documentoIdentificacion")
     String documentoIdentificacion, @WebParam(name = "email")
-    String email, @WebParam(name = "idusuario")
-    int idusuario, @WebParam(name = "logIn")
+    String email, @WebParam(name = "logIn")
     String logIn, @WebParam(name = "nombre")
     String nombre, @WebParam(name = "password")
     String password, @WebParam(name = "tipoUsuario")
     String tipoUsuario) {
-        return ejbRef.crearUsuario(idUsuario, apellido, cc, correoElectronico, documentoIdentificacion, email, idusuario, logIn, nombre, password, tipoUsuario);
+        return ejbRef.crearUsuario(apellido, cc, correoElectronico, documentoIdentificacion, email, logIn, nombre, password, tipoUsuario);
     }
 
 }
